db3f33eb0545a2f76e7d4473f0e5bd4b38b9828c
kiiitkaaa <168138033+kiiitkaaa@users.noreply.github.com>
2024-12-21 15:19:55 +0300

    Add files via upload

---
+++ test_app/test_appTests/test_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  test_appTests.swift
+//  test_appTests
+//
+//  Created by kiiitkaaa on 21.12.2024.
+//
+
+import Testing
+@testable import test_app
+
+struct test_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
